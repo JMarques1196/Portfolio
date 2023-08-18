df1c151bbb48fd67ec3ed8fd87a0013a3e3e0bd7
João André Marques <joaoandremarques@MacBook-Pro.local>
2023-08-19 00:39:31 +0100

    contacts

--- src/containers/Contact/contact.jsx
+++ src/containers/Contact/contact.jsx
@@ -4,9 +4,51 @@ import { client } from "../../client";
 import externalLink from "../../assets/icons/external-link.svg";
 
 const Contacts = () => {
+  const lineNumbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+  ];
+
+  const date = new Date();
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let monthName = month[date.getMonth()];
+  let dayName = dayNames[date.getDay()];
+  let day = date.getDate();
+
+  let currentDate = `${dayName} ${day} ${monthName}`;
+
   const [contactToggle, setContactToggle] = useState([false, false]);
   const [formData, setFormData] = useState({
-    name: "",
+    username: "",
     email: "",
     message: "",
   });
@@ -107,62 +149,150 @@ const Contacts = () => {
             </div>
           ) : null}
         </div>
-        {!isFormSubmitted ? (
-          <form className="inputs-container">
-            <div className="input">
-              <p className="input-title">_name:</p>
-              <input
-                className="input-name"
-                type="text"
-                placeholder=""
-                name="username"
-                value={username}
-                onChange={handleChangeInput}
-              />
-            </div>
-            <div className="input">
-              <p className="input-title">_email:</p>
-              <input
-                className="input-name"
-                type="text"
-                placeholder=""
-                name="email"
-                value={email}
-                onChange={handleChangeInput}
-              />
-            </div>
-            <div className="input">
-              <p className="input-title">_message:</p>
-              <input
-                className="input-message"
-                type="text"
-                placeholder=""
-                name="message"
-                value={message}
-                onChange={handleChangeInput}
-              />
-              <button type="button" className="submit" onClick={handleSubmit}>
-                submit-message
-              </button>
-            </div>
-          </form>
-        ) : (
-          <div className="new-message">
-            <h1 className="new-message-title"> Thank you! </h1>
-            <p className="new-message-text">
-              {" "}
-              Your message was submitted! I'll try to repply as soon as
-              possible!
-            </p>
-            <button
-              type="button"
-              className="submit"
-              onClick={() => setIsFormSubmitted(false)}
-            >
-              Send new message
-            </button>
+        <div className="form-container">
+          <div className="input-title-wrapper">
+            <p className="input-container-title">contacts</p>
           </div>
-        )}
+          <div className="form-content">
+            <div className="border-div">
+              {!isFormSubmitted ? (
+                <form className="form">
+                  <div className="input">
+                    <p className="input-title">_name:</p>
+                    <input
+                      className="input-name"
+                      type="text"
+                      placeholder=""
+                      name="username"
+                      value={username}
+                      onChange={handleChangeInput}
+                    />
+                  </div>
+                  <div className="input">
+                    <p className="input-title">_email:</p>
+                    <input
+                      className="input-name"
+                      type="text"
+                      placeholder=""
+                      name="email"
+                      value={email}
+                      onChange={handleChangeInput}
+                    />
+                  </div>
+                  <div className="input">
+                    <p className="input-title">_message:</p>
+                    <input
+                      className="input-message"
+                      type="text"
+                      placeholder=""
+                      name="message"
+                      value={message}
+                      onChange={handleChangeInput}
+                    />
+                    <button
+                      type="button"
+                      className="submit"
+                      onClick={handleSubmit}
+                    >
+                      submit-message
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <div className="new-message">
+                  <h1 className="new-message-title"> Thank you! </h1>
+                  <p className="new-message-text">
+                    {" "}
+                    Your message was submitted! I'll try to repply as soon as
+                    possible!
+                  </p>
+                  <button
+                    type="button"
+                    className="submit"
+                    onClick={() => setIsFormSubmitted(false)}
+                  >
+                    Send new message
+                  </button>
+                </div>
+              )}
+            </div>
+            <div className="form-animation">
+              <div>
+                {lineNumbers.map((lineNumber, number) => (
+                  <p className="lineNumbers">{lineNumber}</p>
+                ))}
+              </div>
+              <div>
+                <p className="line-code">
+                  <span className="pink">const</span> button{" "}
+                  <span className="pink">=</span> document.querySelector
+                  <span className="gray">(</span>
+                  <span className="orange">'#sendBtn'</span>
+                  <span className="gray">);</span>
+                </p>
+                <div className="spacer" />
+                <p className="line-code">
+                  <span className="pink">const</span> message
+                  <span className="pink"> =</span>
+                  <span className="gray"> {"{"}</span>
+                </p>
+                <p className="line-code">
+                  name:
+                  <span className="orange">
+                    {formData.username === "" ? (
+                      <> ""</>
+                    ) : (
+                      <> "{formData.username}" </>
+                    )}
+                  </span>
+                  <span className="gray">,</span>
+                </p>
+                <p className="line-code">
+                  email:
+                  <span className="orange">
+                    {formData.email === "" ? (
+                      <> ""</>
+                    ) : (
+                      <> "{formData.email}" </>
+                    )}
+                  </span>
+                  <span className="gray">,</span>
+                </p>
+                <p className="line-code">
+                  message:
+                  <span className="orange">
+                    {formData.message === "" ? (
+                      <> ""</>
+                    ) : (
+                      <> "{formData.message}" </>
+                    )}
+                  </span>
+                  <span className="gray">,</span>
+                </p>
+                <p>
+                  <span className="gray">date:</span>
+                  <span className="orange"> "{currentDate}"</span>
+                  <span className="gray">,</span>
+                </p>
+                <div className="spacer" />
+                <p className="line-code">
+                  button.addEventListener(
+                  <span className="orange">'click'</span>
+                  <span className="gray">
+                    , () <span className="pink"> ={">"} </span> {"{"}
+                  </span>
+                  <p className="line-code">
+                    form.send<span className="gray">(</span>message
+                    <span className="gray">);</span>
+                  </p>
+                  <p className="line-code">
+                    <span className="gray">{"}"})</span>
+                  </p>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
